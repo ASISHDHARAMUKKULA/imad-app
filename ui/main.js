3d1929698c224e4a68d5e3f01fd228623a7f6d48
ASISH DHARAMUKKULA <asishdharamukkula@gmail.com>
2018-02-19 22:56:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,7 +26,9 @@ submit.onclick=function(){
     for(var i=0;i<names.length;i++){
         list+='<li>+names[i]'+'</li>';
     }
+    var nameslist=document.getElementById("namelist");
+    names.innerHTML=list;
     
-}
+};
 
 
