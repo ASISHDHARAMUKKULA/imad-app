1a82e5c57cdd294ddc46c0475465d3d6d5524aab
ASISH DHARAMUKKULA <asishdharamukkula@gmail.com>
2018-02-19 21:47:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,19 @@
 var button=document.getElementById("counter");
-var counter=0;
+
 button.onclick=function(){
-    counter=counter+1;
-    var span=document.getElementById("count");
-    span.innerHTML=counter.tostring();
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readystate==XMLHttpRequest.DONE){
+            if(request.status==200){
+                request.responseText;
+                var span=document.getElementById("count");
+                span.innerHTML=counter.tostring();
+            }
+        }
+    };
+    request.open('GET','http://http://asishdharamukkula.imad.hasura-app.io/counter',true);
+    request.send(null);
+   
+    
     
 };
