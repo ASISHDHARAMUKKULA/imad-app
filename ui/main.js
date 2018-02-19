6ff7148198453c7fbf6758546de16eeb9ffa8050
ASISH DHARAMUKKULA <asishdharamukkula@gmail.com>
2018-02-19 21:12:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
-var button=document.getElementById()
+var button=document.getElementById("counter");
+var counter=0;
+button.onClick=function(){
+    counter=counter+1;
+    var span=document.getElementById("count");
+    span.innerHTML=counter.tostring();
+    
+};
